ad953fb8dc6e82fdb793e2c84c24ac590abf7e7a
lynn lin <xiaolulin@yahoo.com>
2015-05-05 13:09:20 +0800

    commit TestArray001

---
+++ src/org/java/array/TestArray001.java
@@ -0,0 +1,10 @@
+public class TestArray001{
+    public static void main (String[] args){
+        int[] a = {2,4,6,7,3,5,1,9,8};
+        for (int i = 0; i <a.length ; i++) {
+            System.out.println(a[i] + "");
+
+        }
+    }
+
+}
